d55012d2626d2dd4a16bba2f8c75153dbb9f711d
Serena Shah <serena.shah@utexas.edu>
2022-05-08 18:06:09 -0500

    starting CRUD

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from jobs import rd, q, add_job, get_job_by_id
 app = Flask(__name__)
 eq_data = {'all_month':[]}
 
-@app.route('/download_data', methods=['POST', 'GET'])
+@app.route('/data', methods=['POST', 'GET'])
 def download_data():
     '''
     loads the data to dictionary of list of dict (easier to work w flask than list)
@@ -70,6 +70,14 @@ def big_earthquake(mag: int):
         if float(rd.hget(item, 'mag')) >= int(mag):
             magnitude_list.append('[ID ' + rd.hget(item, 'id') + ']: ' + rd.hget(item, 'mag'))
     return(f'Magnitudes above {mag}\n' + json.dumps(magnitude_list, indent = 1) + '\n')
+
+@app.route('/delete/<id_num>/<feature_string>', methods =['DELETE'])
+def delete_feature(id_num: str, feature_string:str):
+    feature_list = []
+    for item in rd.keys():
+        if rd.hget(item, 'id') == id_num:
+            rd.delete(item, feature_string)
+    return (f'Earthquake {id_num}-> {feature_string} DELETED.\n')
 
 if __name__ == '__main__':
     app.run(debug=True, host = '0.0.0.0')
