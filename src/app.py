b9279d009eb10c38717ea79d65fcdbeaaf6f207f
Serena Shah <serena.shah@utexas.edu>
2022-05-08 19:10:45 -0500

    added jobs routes

--- src/app.py
+++ src/app.py
@@ -87,6 +87,34 @@ def update_feature(id_num: str, feature_string:str, new_value:str):
             rd.hset(item, feature_string, new_value)
             return (f'Earthquake {id_num}-> {feature_string} UPDATED to {new_value}.\n')
 
+@app.route('/jobs', methods=['POST', 'GET'])
+def jobs_api():
+    """
+    API route for creating a new job to do some analysis. This route accepts a JSON payload
+    describing the job to be created.
+    """
+    if request.method == 'POST':
+        try:
+            job = request.get_json(force=True)
+        except Exception as e:
+            return json.dumps({'status': "Error", 'message': 'Invalid JSON: {}.'.format(e)})
+    
+        return json.dumps(add_job(job['start'], job['end']), indent=2) + '\n'
+
+    elif request.method == 'GET':
+        return """
+  To submit a job, do the following:
+  curl localhost:5028/jobs -X POST -d '{"start":1, "end":2}' -H "Content-Type: application/json"
+"""
+
+@app.route('/jobs/<job_uuid>', methods=['GET'])
+def get_job_result(job_uuid):
+    """
+    API route for checking on the status of a submitted job
+    """
+    return json.dumps(get_job_by_id(job_uuid), indent=2) + '\n'
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host = '0.0.0.0')
